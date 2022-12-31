85e015b6af04d8c48df6b3a7f012166509b63563
Lemonde Shazai <chenglei.chou@gmail.com>
2023-01-01 01:17:23 +0800

    docker: docker init

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { createServer } from "./server";
+import { log } from "logger";
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const port = process.env.PORT || 3001;
+const server = createServer();
+
+server.listen(port, () => {
+  log(`api running on ${port}`);
+});
